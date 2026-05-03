9d7e1c0ef695cd4c3c6546538b633c49165fe015
2b-zipper <119087427+2b-zipper@users.noreply.github.com>
2026-05-03 21:38:20 +0900

    26.6.0のDBスキーマ変更に対応

--- app/src/main/java/app/zipper/knot/utils/LineDBUtils.java
+++ app/src/main/java/app/zipper/knot/utils/LineDBUtils.java
@@ -54,7 +54,17 @@ public class LineDBUtils {
       if (dbFile.exists()) {
         SQLiteDatabase db = SQLiteDatabase.openDatabase(
             dbFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
-        Cursor cursor = db.rawQuery(
+        Cursor cursor =
+            db.rawQuery("SELECT chat_name FROM chats WHERE chat_id = ? LIMIT 1",
+                        new String[] {chatId});
+        if (cursor.moveToFirst()) {
+          String name = cursor.getString(0);
+          cursor.close();
+          db.close();
+          return name;
+        }
+        cursor.close();
+        cursor = db.rawQuery(
             "SELECT chat_name FROM chat_history WHERE chat_id = ? LIMIT 1",
             new String[] {chatId});
         if (cursor.moveToFirst()) {
@@ -131,7 +141,15 @@ public class LineDBUtils {
       if (dbFile.exists()) {
         SQLiteDatabase db = SQLiteDatabase.openDatabase(
             dbFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
-        Cursor cursor = db.rawQuery("SELECT mid FROM profile", null);
+        Cursor cursor = db.rawQuery("SELECT mid FROM user_profile", null);
+        if (cursor.moveToFirst()) {
+          String mid = cursor.getString(0);
+          cursor.close();
+          db.close();
+          return mid;
+        }
+        cursor.close();
+        cursor = db.rawQuery("SELECT mid FROM profile", null);
         if (cursor.moveToFirst()) {
           String mid = cursor.getString(0);
           cursor.close();
